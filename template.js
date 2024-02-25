e20b952967194264d71e0701c4ececf16c1c6065
Pedro Lucas <plucasmsc@gmail.com>
2024-02-25 14:26:06 -0500

    first change

---
+++ template.js
@@ -0,0 +1,13 @@
+export default () => { 
+    return `<!doctype html>
+    <html lang="en"> 
+    <head>
+    <meta charset="utf-8">
+    <title>MAARNNn</title> 
+    </head>
+    <body>
+    <div id="root">Welcome to the server side DressStore Application</div> 
+    </body>
+    </html>` 
+    }
+    
